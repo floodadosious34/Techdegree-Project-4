db9bf6d1ae20afbee244645d61fa7c1051b9718e
James Flood <floodadosious111@gmail.com>
2021-08-17 22:57:59 -0400

    added js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,51 @@ Project 4 - Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {
+    quote: 'Life is what happens when you’re busy making other plans.',
+    source: 'John Lennon',
+    citation: 'Double Fantasy',
+    year: '1980'
+  },
+  {
+    quote: 'Works of art make rules; rules do not make works of art.',
+    source: 'Claude Debussy',
+    citation: 'Companion to Contemporary Musical Thought',
+    year: '1992'
+  },
+  {
+    quote: 'One should either be a work of art, or wear a work of art.',
+    source: 'Oscar Wilde',
+    citation: 'The Soul of Man under Socialism',
+    year: '1891'
+
+  },
+  {
+    quote: 'Melody is the essence of music.',
+    source: 'Wolfgang Amadeus Mozart',
+    citation: 'Reminiscences of Michael Kelly',
+    year: '1826'
+  },
+  {
+    quote: 'For me, music and life are al about style.',
+    source: 'Miles Davis',
+    citation: 'Miles, the Autobiography',
+    year: '1989'
+  },
+  {
+    quote: 'My music is best understood by children and animals',
+    source: 'Igor Stravinsky',
+    citation: 'The Observer',
+    year: '1961'
+  },
+  {
+    quote: 'The song is the center; the song is the key.',
+    source: 'Carole King',
+    citation: 'The Birmingham Post',
+    year: '2001'
+  }
+];
 
 
 
@@ -17,6 +62,15 @@ Project 4 - Random Quote Generator
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote () {
+  const randomNumber = Math.floor( Math.random() * quotes.length );
+  console.log(randomNumber);
+  let result = quotes[randomNumber];
+  console.log(result);
+  return result;
+}
+
+getRandomQuote();
 
 
 /***
